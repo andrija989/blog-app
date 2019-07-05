64200220bec3e21f89fd658b2d22438803a405df
Andrija Perovic <andrija989@gmail.com>
2019-07-05 18:31:32 +0200

    Zadatak 2

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-        
+        <ul>
+            <li v-for="(post,index) in posts" :key="index">
+                <h3>{{post.title}}</h3>
+                <p>{{post.text}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-export default {
+import { postsService } from '@/services/PostsService'
 
+export default {
+    data() {
+        return {
+            posts:[]
+        }
+    },
+
+    created() {
+        postsService.getAll() 
+        .then(response => {
+            this.posts = response.data
+        })
+        .catch(e => {
+            console.log(e)
+        })
+    },
 }
 </script>
 
